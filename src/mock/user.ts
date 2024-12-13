6f40efe5dadee58238b567e7b5d8233533829db4
Fudy-V <fudyV.test@gmail.com>
2024-12-13 22:20:55 +0900

    ✨ userMockの作成

---
+++ src/mock/user.ts
@@ -0,0 +1,9 @@
+export const user1Mock = {
+  id: "1",
+  name: "Takashi",
+  email: "takashi@mail.com",
+  password: "pass1234",
+  roll: "consumer",
+  createdAt: 2024 - 12 - 10,
+  updateAt: 2024 - 12 - 11,
+};
